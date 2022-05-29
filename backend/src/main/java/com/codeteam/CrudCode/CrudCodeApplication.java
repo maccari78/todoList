6b58eb9c5e2cf414042cbaa170db837972d9e08c
Danilo Maccari <maccari78@gmail.com>
2022-05-29 19:19:25 -0300

    other commit

--- backend/src/main/java/com/codeteam/CrudCode/CrudCodeApplication.java
+++ backend/src/main/java/com/codeteam/CrudCode/CrudCodeApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CrudCodeApplication {
-
+        
 	public static void main(String[] args) {
 		SpringApplication.run(CrudCodeApplication.class, args);
 	}
